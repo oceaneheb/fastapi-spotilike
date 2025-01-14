6d1292243ff629b336af15ff33e4809c3b360669
Luigi <luigi.joubert@gmail.com>
2025-01-14 11:39:24 +0100

    Project architecture rework

--- API/main.py
+++ API/main.py
@@ -1,73 +1,29 @@
-from fastapi import FastAPI
-
-from sqlalchemy import ForeignKey, create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from fastapi import FastAPI, HTTPException, Depends
+from typing import Annotated
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from database import SessionLocal, engine
+import models
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-DATABASE_URL = "sqlite:///./spotilike.db"
+origins = [
+	"http://localhost:3000"
+]
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=origins,
+)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+def get_db():
+	db = SessionLocal()
+	try:
+		yield db
+	finally:
+		db.close()
 
-Base = declarative_base()
+db_dependency = Annotated[Session, Depends(get_db)]
 
-class User(Base):
-	__tablename__ = "users"
-
-	id = Column(Integer, primary_key=True, index=True)
-	name = Column(String)
-	email = Column(String)
-	password = Column(String)
-
-class Artist(Base):
-    __tablename__ = "artists"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    avatar = Column(String)
-    biography = Column(String)
-    albums = relationship("Album", back_populates="artist") # One-to-many
-    songs = relationship("Song", back_populates="artist") # One-to-many
-
-class Song(Base):
-    __tablename__ = "songs"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    artist_id = Column(Integer, ForeignKey("artists.id"))
-    album_id = Column(Integer, ForeignKey("albums.id"))
-    year = Column(Integer)
-    artist = relationship("Artist", back_populates="songs") # Many-to-One
-    album = relationship("Album", back_populates="songs") # Many-to-One
-    genres = relationship("Genre", secondary="song_genre", back_populates="songs") # Many-to-many relationship with Genre through the association table
-
-class Album(Base):
-    __tablename__ = "albums"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    cover = Column(String)
-    artist_id = Column(Integer, ForeignKey("artists.id"))
-    release_date = Column(String)
-    artist = relationship("Artist", back_populates="albums") # Many-to-One
-    songs = relationship("Song", back_populates="album") # One-to-many
-
-# Genre table
-class Genre(Base):
-    __tablename__ = "genres"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    songs = relationship("Song", secondary="song_genre", back_populates="genres") # Many-to-many
-
-# Association table for many-to-many relationship between Song and Genre
-class SongGenre(Base):
-    __tablename__ = "song_genre"
-
-    song_id = Column(Integer, ForeignKey("songs.id"), primary_key=True)
-    genre_id = Column(Integer, ForeignKey("genres.id"), primary_key=True)
-    
-Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
---
+++ API/models.py
@@ -0,0 +1,59 @@
+from database import Base
+from sqlalchemy import ForeignKey, Column, Integer, String
+from sqlalchemy.orm import relationship
+
+class User(Base):
+	__tablename__ = "users"
+
+	id = Column(Integer, primary_key=True, index=True)
+	name = Column(String)
+	email = Column(String)
+	password = Column(String)
+
+class Artist(Base):
+    __tablename__ = "artists"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    avatar = Column(String)
+    biography = Column(String)
+    albums = relationship("Album", back_populates="artist") # One-to-many
+    songs = relationship("Song", back_populates="artist") # One-to-many
+
+class Song(Base):
+    __tablename__ = "songs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    artist_id = Column(Integer, ForeignKey("artists.id"))
+    album_id = Column(Integer, ForeignKey("albums.id"))
+    year = Column(Integer)
+    artist = relationship("Artist", back_populates="songs") # Many-to-One
+    album = relationship("Album", back_populates="songs") # Many-to-One
+    genres = relationship("Genre", secondary="song_genre", back_populates="songs") # Many-to-many relationship with Genre through the association table
+
+class Album(Base):
+    __tablename__ = "albums"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    cover = Column(String)
+    artist_id = Column(Integer, ForeignKey("artists.id"))
+    release_date = Column(String)
+    artist = relationship("Artist", back_populates="albums") # Many-to-One
+    songs = relationship("Song", back_populates="album") # One-to-many
+
+class Genre(Base):
+    __tablename__ = "genres"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    songs = relationship("Song", secondary="song_genre", back_populates="genres") # Many-to-many
+
+# Association table for many-to-many relationship between Song and Genre
+class SongGenre(Base):
+    __tablename__ = "song_genre"
+
+    song_id = Column(Integer, ForeignKey("songs.id"), primary_key=True)
+    genre_id = Column(Integer, ForeignKey("genres.id"), primary_key=True)
+    
