29be4124ae16c63744ad5eadc609ca2b15041a28
Luigi <luigi.joubert@gmail.com>
2025-01-14 09:45:46 +0100

    Base file with sqlite and users table

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+app = FastAPI()
+
+DATABASE_URL = "sqlite:///./spotilike.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+class User(Base):
+	__tablename__ = "users"
+
+	id = Column(Integer, primary_key=True, index=True)
+	name = Column(String)
+	email = Column(String)
+	password = Column(String)
+
+Base.metadata.create_all(bind=engine)
