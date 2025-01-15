223001ec497dd296480b994cd2a6d4fbf7007adf
Océane Hebrard <oceane47.heb@gmail.com>
2025-01-15 10:16:25 +0100

    ajout de nouveaux endpoints

--- API/main.py
+++ API/main.py
@@ -28,15 +28,33 @@ class UserModel(UserBase):
 	class Config:
 		orm_mode = True
 
+class ArtistBase(BaseModel):
+	name: str
+	avatar: str
+	biography: str
+
+class ArtistModel(ArtistBase):
+	id: int
+
+	class Config:
+		orm_mode = True
+
 class AlbumBase(BaseModel):
 	title: str
 	cover: str
 	artist_id: int
 	release_date: str
+	artist: ArtistBase
 
 class AlbumModel(AlbumBase):
 	id: int
 
+	class Config:
+		orm_mode = True
+
+class GenreBase(BaseModel):
+	name: str
+class GenreModel(GenreBase):
 	class Config:
 		orm_mode = True
 
@@ -63,9 +81,64 @@ async def create_user(user: UserBase, db: db_dependency):
 	db.refresh(db_user)
 	return db_user
 
+# Supprimer un utilisateur par id
+@app.delete("/users/{user_id}", response_model=UserModel)
+async def delete_user(user_id: int, db: db_dependency):
+	db_user = db.query(models.User).filter(models.User.id == user_id).first()
+	if db_user is None:
+		raise HTTPException(status_code=404, detail="Utilisateur non trouvé")
+	db.delete(db_user)
+	db.commit()
+	return db_user
+
 # Albums
 # Récupère la liste de tous les albums
-@app.get("/users", response_model=List[AlbumModel])
-async def read_albums(db: db_dependency, skip: int = 0, limit: int = 100):
-	albums = db.query(models.Album).offset(skip).limit(limit).all()
+@app.get("/albums/", response_model=List[AlbumModel])
+async def get_albums(db: db_dependency):
+	albums = db.query(models.Album).all()
 	return albums
+
+# Récupère les détails de l'album précisé par id
+@app.get("/albums/{album_id}", response_model=AlbumModel)
+async def get_album(album_id: int, db: db_dependency):
+	album = db.query(models.Album).filter(models.Album.id == album_id).first()
+	if not album:
+		raise HTTPException(status_code=404, detail="Aucun album trouvé")
+	return album
+
+# Ajout d'un album
+@app.post("/albums/", response_model=AlbumModel)
+async def create_album(album: AlbumBase, db: db_dependency):
+	db_album = models.Album(**album.dict())
+	db.add(db_album)
+	db.commit()
+	db.refresh(db_album)
+	return db_album
+
+# Supprimer un album par id
+@app.delete("/albums/{album_id}", response_model=AlbumModel)
+async def delete_album(album_id: int, db: db_dependency):
+	db_album = db.query(models.Album).filter(models.Album.id == album_id).first()
+	if db_album is None:
+		raise HTTPException(status_code=404, detail="Album non trouvé")
+	db.delete(db_album)
+	db.commit()
+	return db_album
+
+# Genres
+# Récupère la liste de tous les genres
+@app.get("/genres", response_model=List[GenreModel])
+async def get_genres(db: db_dependency):
+	genres = db.query(models.Genre).all()
+	return genres
+
+# Artists
+# Supprimer un artiste par id
+@app.delete("/artists/{artist_id}", response_model=ArtistModel)
+async def delete_artist(artist_id: int, db: db_dependency):
+	db_artist = db.query(models.Artist).filter(models.Artist.id == artist_id).first()
+	if db_artist is None:
+		raise HTTPException(status_code=404, detail="Artiste non trouvé")
+	db.delete(db_artist)
+	db.commit()
+	return db_artist
